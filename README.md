b461c1a506d453bb96b5a7bc322fad7d3c008069
VeranikaBuzhynskaya <fruittelllla@gmail.com>
2018-01-31 07:05:22 +0300

    Add logger for app

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# fronCampNode
+# frontCampNode
+
+#### For task "Node.js and Express.js part 1":
+1. npm install;
+2. npm start;
--- app/server.js
+++ app/server.js
@@ -1,23 +1,33 @@
 import express from 'express';
 import blogsRouter from './components/blogs/blogs.router';
+import logger from './utils/logger';
 import path from 'path';
 
 const app = express();
+const port = 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.get('/', function (req, res) {
-  res.render('welcomePage', { title: 'Hey', message: 'Hello there!' })
-});
+// app.get('/', function (req, res) {
+//   res.render('welcomePage', { title: 'Hey', message: 'Hello there!' })
+// });
 
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.use('/blogs', blogsRouter);
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.use((req, res, next) => {
+  logger.info(`Route ${req.url}`);
+  next();
 });
 
-export default app;
+app.use('/blogs', blogsRouter);
+
+app.use((req, res) => {
+  logger.warn(`Route ${req.url} is not to defined!`);
+  res.render('welcomePage', { title: 'Hey', message: 'Hello there!' })
+});
+
+app.listen(port, function () {
+  logger.info(`App listening on port ${port}!`)
+});
